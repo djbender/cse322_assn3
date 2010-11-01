2617b60ee4cc0daa2a460027742381fd5446ebdb
Derek Bender <nofeardjb@gmail.com>
2010-11-01 15:44:00 -0500

    updating some stuff

--- assn3_part2.hs
+++ assn3_part2.hs
@@ -2,6 +2,7 @@ import Prelude
 import System ( getArgs )
 import List
 import Maybe
+import Data.List
 
 -- The main method that will be used for testing / command line access.
 main = do
@@ -28,3 +29,18 @@ yourMain track =
 	partTwoOutput (createOval track)
 
 -- createOval :: [[Int]] -> [[Int]]
+createOval list = createOval_ (permutations list)
+
+createOval_ list
+	| isOval (head list) = (head list)
+	| otherwise = createOval_ (tail list)
+
+isOval list = (take (length (fixed list) `div` 2) (fixed list)) == (reverse (take (length (fixed list) `div` 2) (reverse (fixed list))))
+
+fixed list = readjust (firsts list)
+
+firsts list = map head list
+
+readjust (x:xs)  
+	| x == 0 = (x:xs)
+	| otherwise = readjust (xs ++ [x])
