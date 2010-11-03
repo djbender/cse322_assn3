be8ba24a828027f7a8c4403c5e9015412a51d96c
Derek Bender <nofeardjb@gmail.com>
2010-11-03 17:21:48 -0500

    updated a log of logic in isOval.hs

--- isOval.hs
+++ isOval.hs
@@ -1,12 +1,37 @@
 import Prelude
 import List
 
-isOval list = (take (length (fixed list) `div` 2) (fixed list)) == (reverse (take (length (fixed list) `div` 2) (reverse (fixed list))))
+--isOval list = (take (length (fixed list) `div` 2) (fixed list)) == (reverse (take (length (fixed list) `div` 2) (reverse (fixed list))))
+
+--isOval list 
+-- need something that errors on empty lists or lists with less than 4 curves (0).
+
+thirdSide list = take ((positionSecondZero list)+1) (drop (positionThirdZero list) list)
+
+secondSide list = take ( (drop (positionSecondZero list) list)
+
+firstSide list = take (positionFirstZero list) list 
+
+positionFourthZero [] = 0
+positionFourthZero list = (positionFirstZero list) + (positionFirstZero (drop (positionFirstZero list) list)) + (positionFirstZero (drop (positionSecondZero list) list)) + (positionFirstZero (drop (positionThirdZero list) list))
+
+positionThirdZero [] = 0
+positionThirdZero list = (positionFirstZero list) + (positionFirstZero (drop (positionFirstZero list) list)) + (positionFirstZero (drop (positionSecondZero list) list))
+
+--positionSecondZero list = positionFirstZero (drop (positionFirstZero list) list)
+positionSecondZero [] = 0
+positionSecondZero list = (positionFirstZero list) + (positionFirstZero (drop (positionFirstZero list) list))
+
+positionFirstZero [] = 0
+positionFirstZero (x:xs) 
+	| x == 0 = 1
+	| otherwise = 1 + (positionFirstZero xs)
 
 fixed list = readjust (firsts list)
 
 firsts list = map head list
 
+--take a list an shift elements to the tail one by one until a zero is in the last place.
 readjust (x:xs)  
-	| x == 0 = (x:xs)
+	| x == 0 = (xs ++ [x]) --one last time
 	| otherwise = readjust (xs ++ [x])
